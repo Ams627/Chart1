454cc2c3b3a1df57463bf9c5d29ba63f5217efeb
Adrian Sims <asims@parkeon.com>
2017-11-07 16:36:53 +0000

    Add project files.

---
+++ Chart1/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Chart1
+{
+    class ViewModel
+    {
+        public ObservableCollection<int> TransactionCount { get; set; } = new ObservableCollection<int>();
+        public ViewModel()
+        {
+            var con = new OracleConnection("Data Source=" + "raildev:1521/ukrail" + ";User Id=" + "HexSnap2017" + ";Password=" + "hexsnap2017");
+            con.Open();
+            var cmd = new OracleCommand("select month, count(1) from (select to_char(trans_date, 'yyyy-mm') month from transaction) group by month order by month", con);
+            OracleDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                var month = dr.GetString(0);
+                var count = dr.GetInt32(1);
+                System.Diagnostics.Debug.WriteLine($"month: {month} count: {count}");
+                TransactionCount.Add(count);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,18 @@
+rem echo off
+echo done >c:\temp\gitdone.txt
+git init Chart1
+cd Chart1
+git add Chart1/App.xaml
+git add Chart1/App.xaml.cs
+git add Chart1/App.config
+git add Chart1/MainWindow.xaml
+git add Chart1/MainWindow.xaml.cs
+git add Chart1/Properties/AssemblyInfo.cs
+git add Chart1/app.config
+git add Chart1/Properties/Settings.settings
+git add Chart1/Properties/Resources.Designer.cs
+git add Chart1/Properties/Resources.resx
+git add Chart1/Properties/Settings.Designer.cs
+git add Chart1/Chart1.csproj
+git add Chart1.sln
+git add .gitignore
